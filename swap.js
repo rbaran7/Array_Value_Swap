6a9c2327e9abd5f7c952797b24a7349392c2d5dc
Roman Baran <roman.baran@vexterra.com>
2018-03-12 18:24:40 -0400

    Added express and created a simple post test. On to building the front end now

--- swap.js
+++ swap.js
@@ -1,18 +1,30 @@
 'use strict';
 
 const readline = require('readline');
-      
-const rl = readline.createInterface({
+const express = require('express');
+const bp = require('body-parser');
+  
+
+let app = express();
+    app.use(bp.json());
+    app.use(bp.urlencoded({extended: true}));
+//let arr = [],
+  //  swapNums = [],
+//    i = 1;
+    
+app.post('/app/list', (req, res) =>{
+   console.log(req.body.data); 
+   res.send('All good man');
+});
+
+app.listen(6000);
+/*const rl = readline.createInterface({
         input: process.stdin,
         output: process.stdout
-      });
-
-let arr = [],
-    swapNums = [],
-    i = 1;
+      });*/
 
 //Function that generates the list of values. Checkes for duplicates, list can be a mix of ints and strings.
-function getUserInput(){
+/*function getUserInput(){
     rl.question('Value'+' '+i+': ', (answer) =>{
         if(arr.includes(answer)){
             console.log('\nWoops! Sorry, no duplicate values. Please try again.\n');
@@ -31,9 +43,10 @@ function getUserInput(){
         }
       }
    }); 
-};
+};*/
 
 //Function that gets the two values to swap from the user. Swaps the values and prints the new ordered list.
+/*
 function swapValues(numericList){
     console.log('Please enter one of the values from the list you wish to swap:\n')
     rl.question('Value:', (answer) =>{
@@ -59,7 +72,8 @@ function swapValues(numericList){
         }
     });
 };
+*/
 
 //Start it off.
-console.log('\nPlease enter 10 values below:\n\n');
-getUserInput(i);
+//console.log('\nPlease enter 10 values below:\n\n');
+//getUserInput(i);
