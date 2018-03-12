2b879078865f8fbbb8eb7d42101ae04775e309d2
Roman Baran <roman.baran@vexterra.com>
2018-03-12 18:43:38 -0400

    Added base for React

---
+++ arrayvalueswap/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+let destination = document.querySelector("#container");
+
+ReactDOM.render(
+    <div>
+        <p>Here we go!</p>
+    </div>,
+    destination
+);
